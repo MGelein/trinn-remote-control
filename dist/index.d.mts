cff38ce6c0a509d32e54be4037ea8c8fb5904430
Mees Gelein <meesgelein@gmail.com>
2023-11-10 23:15:46 +0100

    new build

--- dist/index.d.mts
+++ dist/index.d.mts
@@ -1,5 +1,28 @@
-declare class TRINNDemo {
-    constructor();
+import { DataConnection, Peer } from 'peerjs';
+
+type TRINNError = {
+    name: string;
+    message: string;
+    type: string;
+};
+declare class TRINNPeer {
+    peer: Peer;
+    id: string | undefined;
+    error: TRINNError | undefined;
+    constructor(requiredId: string);
+}
+declare class TRINNController extends TRINNPeer {
+    connection: DataConnection | undefined;
+    constructor(sharedId: string);
+    sendPress(keyName: string): void;
+    sendRelease(keyName: string): void;
+}
+declare class TRINNRemote extends TRINNPeer {
+    private pressCallback;
+    private releaseCallback;
+    constructor(sharedId: string);
+    onPress(onPressCallback: (key: string) => void): void;
+    onRelease(onReleaseCallback: (key: string) => void): void;
 }
 
-export { TRINNDemo };
+export { TRINNController, TRINNError, TRINNRemote };
--- dist/index.mjs
+++ dist/index.mjs
@@ -1,10 +1,56 @@
 // src/index.ts
-var TRINNDemo = class {
-  constructor() {
-    console.log("it work");
+import { Peer } from "peerjs";
+var TRINNPeer = class {
+  constructor(requiredId) {
+    this.peer = new Peer(requiredId);
+    this.peer.on("open", (id) => {
+      this.id = id;
+    });
+    this.peer.on("error", ({ message, type, name }) => {
+      this.error = { message, type, name };
+    });
+  }
+};
+var TRINNController = class extends TRINNPeer {
+  constructor(sharedId) {
+    super(`${sharedId}-controller`);
+    this.peer.on("open", (id) => {
+      this.id = id;
+      this.connection = this.peer.connect(`${sharedId}-remote`);
+    });
+  }
+  sendPress(keyName) {
+    var _a;
+    (_a = this.connection) == null ? void 0 : _a.send({ key: keyName, type: "press" });
+  }
+  sendRelease(keyName) {
+    var _a;
+    (_a = this.connection) == null ? void 0 : _a.send({ key: keyName, type: "release" });
+  }
+};
+var TRINNRemote = class extends TRINNPeer {
+  constructor(sharedId) {
+    super(`${sharedId}-remote`);
+    this.peer.on("connection", (connection) => {
+      connection.on("data", (data) => {
+        var _a, _b;
+        const { key, type } = data;
+        if (type === "press")
+          (_a = this.pressCallback) == null ? void 0 : _a.call(this, key);
+        if (type === "release")
+          (_b = this.releaseCallback) == null ? void 0 : _b.call(this, key);
+      });
+    });
+  }
+  onPress(onPressCallback) {
+    this.pressCallback = onPressCallback;
+  }
+  onRelease(onReleaseCallback) {
+    this.releaseCallback = onReleaseCallback;
   }
 };
 export {
-  TRINNDemo
+  TRINNController,
+  TRINNRemote
 };
 //# sourceMappingURL=index.mjs.map
